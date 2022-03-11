06a999a077ac59436d3716e59eebc1f7053ef527
Adolfo <adolfo.oes@gmail.com>
2022-03-11 11:20:45 -0300

    implemented firebase utils

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -23,13 +23,31 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser : user });
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // createUserProfileDocument(user)
+
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser : {
+              id : snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state)
+        });
+      }
+
+      this.setState({currentUser : userAuth})
+
     })
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
@@ -37,7 +55,7 @@ class App extends React.Component {
     return (
       <div>
 
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
 
         <Switch>
           <Route exact path='/' component={HomePage} />
